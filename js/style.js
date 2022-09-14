371a7136cd0cde19d6d3c26d85d87ea17b36735a
Lee dong ho <dong963939@gmail.com>
2022-09-14 16:23:42 +0900

    ⚡️ 헤더 부분 js 수정함.

--- js/style.js
+++ js/style.js
@@ -1,74 +1,74 @@
-$(function() {
+$(function () {
     //모바일 메뉴 전체화면
-    $('header>.innerBox>.mobileheader>a').on('click', function(){
+    $('header>.innerBox>.mobileheader>a').on('click', function () {
         $('.app')
-        .addClass("mobileActive")
-        .siblings();
+            .addClass("mobileActive")
+            .siblings();
     });
-    $('.app>.sidebar>div>i').on('click', function(){
+    $('.app>.sidebar>div>i').on('click', function () {
         $('.app')
-        .removeClass("mobileActive")
-        .siblings();
+            .removeClass("mobileActive")
+            .siblings();
     });
 
     //검색창 클릭시 검색창 나오게
     $("header>.innerBox>.main-menu>.headerIcon>a:first-child").click(function (e) {
         $("#search")
-        .removeClass("active")
-        .siblings();
+            .removeClass("active")
+            .siblings();
     })
     $("#search>.search-header>.btn-close").click(function (e) {
         $("#search")
-        .addClass("active")
+            .addClass("active")
             .siblings();
     })
 
     //헤더 검색창 클릭시 검색창 나오게
     $("header>.innerBox>.mobileheader>.MobileIcon>a:first-child").click(function (e) {
         $("#search")
-        .removeClass("active")
-        .siblings();
+            .removeClass("active")
+            .siblings();
     })
     $("#search>.search-header>.btn-close").click(function (e) {
         $("#search")
-        .addClass("active")
-        .siblings();
+            .addClass("active")
+            .siblings();
     })
 
     //모바일 사람 이미지 클릭시 메뉴 나오도록
-    $(".Icon_last").hover(function(){
+    $(".Icon_last").hover(function () {
         $(".Mobile_UserBox").addClass("bg");
-    }, function(){
+    }, function () {
         $(".Mobile_UserBox").removeClass("bg");
     })
-    $(".Mobile_UserBox").hover(function(){
+    $(".Mobile_UserBox").hover(function () {
         $(this).addClass("bg");
-    }, function(){
+    }, function () {
         $(this).removeClass("bg");
     })
 
     //모바일 화면 나오게 만들기
-    $('.mobileheader>a>i').click(function(){
+    $('.mobileheader>a>i').click(function () {
         $('body').toggleClass('menu-open');
     })
 
     //헤더 사람 이미지 클릭시 메뉴 나오도록
-    $(".headerIcon>.Icon_last").hover(function(){
+    $(".headerIcon>.Icon_last").hover(function () {
         $(".header_UserBox").addClass("bg1");
-    }, function(){
+    }, function () {
         $(".header_UserBox").removeClass("bg1");
     })
-    $(".header_UserBox").hover(function(){
+    $(".header_UserBox").hover(function () {
         $(this).addClass("bg1");
-    }, function(){
+    }, function () {
         $(this).removeClass("bg1");
     })
 
     //검색창 클릭시 2자 이상 입력
-    $('form[name="search_form"]').on('submit', function(e){
+    $('form[name="search_form"]').on('submit', function (e) {
         var keyword = $('input[name="s"]', this).val();
 
-        if ( keyword.split(' ').join('')  == '' || keyword.split(' ').join('').length < 2 ) {
+        if (keyword.split(' ').join('') == '' || keyword.split(' ').join('').length < 2) {
             alert('검색어를 2자 이상 입력해주세요.');
             e.preventDefault();
             return false;
@@ -77,14 +77,16 @@ $(function() {
 
     $('#menu-item-57').css('font-weight', 900);
 
-    //스크롤 탑 내리면 나오게
+    /**
+     * ? 스크롤을 내렸을 시 하얀 배경색이 나오도록
+     */
     const nav = document.querySelector('header');
 
-    function stickMenu(){
+    function stickMenu() {
         let winYPos = window.pageYOffset;
         let navElePos = nav.offsetTop;
 
-        if(winYPos > navElePos) {
+        if (winYPos > navElePos) {
             nav.classList.add('sticky');
         } else {
             nav.classList.remove('sticky');
@@ -93,7 +95,25 @@ $(function() {
 
     window.addEventListener('scroll', stickMenu);
 
-    // //메뉴 호버시 슬라이드 다운 효과
+    /**
+     * ? 스크롤을 내렸을때 #header의 CSS 속성중 top 값을 -100px로 변경했다가 제거한다.
+     */
+    var lastScrollTop = 0, delta = 15;
+
+    $(window).scroll(function (e) {
+        var st = $(this).scrollTop();
+
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+        if ((st > lastScrollTop) && (lastScrollTop > 0)) {
+            $("header").css("transform", "translateY(-100%)");
+        } else {
+            $("header").css("transform", "translateY(0%)");
+        }
+        lastScrollTop = st;
+    });
+
+    // 메뉴 호버시 슬라이드 다운 효과
     const main = document.querySelectorAll(".main-menu > li");
 
     for (let i = 0; i < main.length; i++) {
@@ -112,12 +132,11 @@ $(function() {
         speed: 300,
         infinite: true,
         autoplay: true,
-        // arrows: true,
         dots: true,
-        pauseOnHover : true,
-        draggable : true,
-        prevArrow : $('.main_indicator .prevArrow'),
-        nextArrow : $('.main_indicator .nextArrow'),
+        pauseOnHover: true,
+        draggable: true,
+        prevArrow: $('.main_indicator .prevArrow'),
+        nextArrow: $('.main_indicator .nextArrow'),
     });
 
     //스타벅스 메뉴 슬라이드
@@ -129,8 +148,8 @@ $(function() {
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        pauseOnHover : true,
-        draggable : true,
+        pauseOnHover: true,
+        draggable: true,
     });
 
     // 스타벅스 이벤트
@@ -141,8 +160,8 @@ $(function() {
         slidesToShow: 4,
         slidesToScroll: 2,
         autoplay: true,
-        prevArrow : $('.indicator .prevArrow'),
-        nextArrow : $('.indicator .nextArrow'),
+        prevArrow: $('.indicator .prevArrow'),
+        nextArrow: $('.indicator .nextArrow'),
         responsive: [
             {
                 breakpoint: 1024,
